b23d9c2c3e064833db103bf961e7e07e1c8ed5cf
everaldo@bb-8 <juniorklawa10@gmail.com>
2021-05-20 15:00:11 -0300

    first commit

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import { Counter } from 'react-native-auto-size-text'
+
+const App = () => {
+  useEffect(() => {
+    console.log('')
+  })
+
+  return <Counter />
+}
+
+export default App
